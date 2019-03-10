81f161a2df120253adbb921b841dc4cc2b83a02a
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-10 19:20:14 +0530

    Update the import statements

--- Student-Recommendation-System/src/app/app.module.ts
+++ Student-Recommendation-System/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { MaterialImportsModule } from './material-imports.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RegistrationFormComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+  BrowserModule,
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  ReactiveFormsModule,
+  MaterialImportsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  bootstrap: [AppComponent],
+  })
+export class AppModule {}
