778c826fb55d9dd5a0334944078b6bec37e0ecb3
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-12 10:39:51 +0530

    Add new routes in the app

--- Student-Recommendation-System/src/app/app-routing.module.ts
+++ Student-Recommendation-System/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AcadFormComponent } from './acad-form/acad-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registration', component: RegistrationFormComponent },
+  { path: 'acadform', component: AcadFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  exports: [RouterModule],
+  })
+export class AppRoutingModule {}
