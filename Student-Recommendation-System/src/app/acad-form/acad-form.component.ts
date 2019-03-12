462cb0378ea5fe31569f87522987d6a251c693c5
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-12 10:43:10 +0530

    Add basic Stepper form for academic details registration

--- Student-Recommendation-System/src/app/acad-form/acad-form.component.ts
+++ Student-Recommendation-System/src/app/acad-form/acad-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder, FormGroup, FormArray, FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-acad-form',
   templateUrl: './acad-form.component.html',
-  styleUrls: ['./acad-form.component.css']
-})
+  styleUrls: ['./acad-form.component.css'],
+  })
 export class AcadFormComponent implements OnInit {
+  matriculation = this.fb.group({
+    percentage: [''],
+  });
 
-  constructor() { }
+  puc = this.fb.group({
+    percentage: [''],
+    stream: [''],
+  });
 
-  ngOnInit() {
-  }
+  university = this.fb.group({
+    stream: [''],
+    percentage: [''],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {}
 }
