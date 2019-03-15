76530f1d21582292d064bad57beac8e1434e0da5
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-15 09:37:02 +0530

    Add basic server logic

---
+++ Student-Recommendation-System/server.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+// Start point of the server
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+// Routes
+const loginRouter = require('./Routes/login.route');
+
+// Server
+const app = express();
+const distPath = path.join(__dirname, '/Views');
+mongoose.Promise = global.Promise;
+const db = mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/test', {
+  useNewUrlParser: true,
+}).connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connetion to DB up!');
+  app.use(cors());
+  app.use(express.static(distPath));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use('/login', loginRouter);
+
+  // app.get("/", (req, res) => {
+  //   res.sendFile(distPath + "index.html");
+  // });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`listening on ${process.env.PORT}`);
+  });
+});
