64414fcc11057f8d9fa1a097140558382022339c
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-17 21:19:05 +0530

    Add dashboard route

--- Student-Recommendation-System/src/app/app-routing.module.ts
+++ Student-Recommendation-System/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AcadFormComponent } from './acad-form/acad-form.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'registration', component: RegistrationFormComponent },
   { path: 'acadform', component: AcadFormComponent },
-  { path: 'login', component: LoginComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
