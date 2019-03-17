c47cbb34ac4ea84477a9f2485b2e3cff2e9282ce
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-17 23:32:48 +0530

    Add dashboard service

---
+++ Student-Recommendation-System/src/app/dashboard.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Details } from './Details';
+
+@Injectable({
+  providedIn: 'root',
+  })
+export class DashboardService {
+  constructor(private http: HttpClient) {}
+
+  private apiUrl = 'http://localhost:3000/dashboard/:id';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'my-auth-token',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+      'Access-Control-Allow-Origin': '*',
+    }),
+  };
+
+  // URL to web api
+  getDashboard(): Observable<Details> {
+    return this.http.get<Details>(this.apiUrl, { headers: this.httpOptions.headers }).pipe(
+      tap(_ => console.log('Got response')),
+      catchError(this.handleError<Details>('getDashboard')),
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
---
+++ Student-Recommendation-System/src/app/details.ts
@@ -0,0 +1,11 @@
+export class Details {
+  user: String;
+
+  firstName: String;
+
+  lastName: String;
+
+  Mobile: String;
+
+  email: String;
+}
