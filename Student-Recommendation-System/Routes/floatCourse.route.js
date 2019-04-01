8c944eda11681da3bd4635cd8ca8148cb1c4bc63
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-01 12:31:58 +0530

    Add the floatCourse Router

--- Student-Recommendation-System/Routes/floatCourse.route.js
+++ Student-Recommendation-System/Routes/floatCourse.route.js
@@ -3,5 +3,5 @@ import { Router } from 'express';
 const callback = require('../Controllers/floatCourse.controller');
 
 const router = Router();
-router.post('/add', callback.confirm);
+router.post('/add', callback.addCourse);
 module.exports = router;
