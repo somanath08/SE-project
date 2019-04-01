063954998c834a6e41f041914aa91796b98211b5
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-01 12:03:22 +0530

    Add form controls and validations

--- Student-Recommendation-System/src/app/float-course/float-course.component.ts
+++ Student-Recommendation-System/src/app/float-course/float-course.component.ts
@@ -1,52 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {Course } from '../course-details';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { FloatCourseService } from '../float-course.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Course } from '../course-details';
+import { FloatCourseService } from '../float-course.service';
 @Component({
   selector: 'app-float-course',
   templateUrl: './float-course.component.html',
-  styleUrls: ['./float-course.component.css']
-})
-export class FloatCourseComponent{
+  styleUrls: ['./float-course.component.css'],
+  })
+export class FloatCourseComponent {
+  constructor(private floatCourseService: FloatCourseService, private router: Router) {}
 
-  constructor( 
-    private floatCourseService: FloatCourseService,
-    private router: Router,
-    ) {
-   }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-  course : Course[]=[
-    {value: 'lab', viewValue: 'Lab'},
-    {value: 'theory', viewValue: 'Theory'},
+  course: Course[] = [{ value: 'lab', viewValue: 'Lab' }, { value: 'theory', viewValue: 'Theory' }];
+
+  prerequisites: String[] = [
+    'Programming',
+    'Algorithms and DS',
+    'Computer Networks',
+    'Operating System',
+    'Distributed Computing',
+    'Parallel Computing',
   ];
-  prerequisites : String[]=['Programming', 'Algorithms and DS', 'Computer Networks', 'Operating System', 'Distributed Computing', 'Parallel Computing'];
 
-  credits : Course[]=[
-    {value: '2', viewValue: 'Two'},
-    {value: '3', viewValue: 'Three'},
-    {value: '4', viewValue: 'Four'},
+  credits: Course[] = [
+    { value: '2', viewValue: 'Two' },
+    { value: '3', viewValue: 'Three' },
+    { value: '4', viewValue: 'Four' },
   ];
+
   profileForm = new FormGroup({
-    courseType: new FormControl(''),
+    courseType: new FormControl('', Validators.required),
     coursename: new FormControl('', [
       Validators.required,
-      Validators.pattern(/^[A-Z||a-z]$/),
+      Validators.pattern(/^[A-Z||a-z||0-9||\s||-]+$/),
     ]),
     credits: new FormControl(''),
-    //prerequisite: new FormControl(''),
-   });
-   onSubmit() {
-    // TODO: Use EventEmitter with form value
+    prerequisite: new FormControl(''),
+  });
+
+  onSubmit() {
     console.log(this.profileForm.value);
-    this.floatCourseService.floatcourse(this.profileForm.value).subscribe((status) => {
-      if (status === 'Saved') {
-        console.log(status);
-        this.router.navigate(['/dashboard']);
-      }
-    });
   }
-  
 }
