77ec620a845a1902256082092e0ef9c24ee5d6c1
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-07 12:40:58 +0530

    Remove deleted component

--- Student-Recommendation-System/src/app/app.module.ts
+++ Student-Recommendation-System/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule,
+} from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
@@ -11,13 +19,19 @@ import { MaterialImportsModule } from './material-imports.module';
 import { AcadFormComponent } from './acad-form/acad-form.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
-import { LayoutModule } from '@angular/cdk/layout';
 import { FloatCourseComponent } from './float-course/float-course.component';
 import { DialougeComponent } from './dialouge/dialouge.component';
 
 @NgModule({
-  declarations: [AppComponent, RegistrationFormComponent, AcadFormComponent, LoginComponent, DashboardComponent, FloatCourseComponent, DialougeComponent],
+  declarations: [
+  AppComponent,
+  RegistrationFormComponent,
+  AcadFormComponent,
+  LoginComponent,
+  DashboardComponent,
+  FloatCourseComponent,
+  DialougeComponent,
+  ],
   entryComponents: [DialougeComponent],
   imports: [
   BrowserModule,
@@ -31,7 +45,7 @@ import { DialougeComponent } from './dialouge/dialouge.component';
   MatMenuModule,
   MatIconModule,
   MatButtonModule,
-  LayoutModule
+  LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
