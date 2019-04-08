de0b7805065f516148e62ec287e913c6577e0967
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-08 23:09:45 +0530

    Add route for advisor api

--- Student-Recommendation-System/Routes/advisor.route.js
+++ Student-Recommendation-System/Routes/advisor.route.js
@@ -4,4 +4,5 @@ const callback = require('../Controllers/advisor.controller');
 
 const router = Router();
 router.get('/start/:id', callback.getAdiveForSemStart);
+router.get('/mid/:user/:id/:grade', callback.getAdiveForMidSem);
 module.exports = router;
