0ef97bbdfadaf80cf6a1d743be6f520cf35851a2
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-08 23:44:55 +0530

    Add bolier plate code

---
+++ Student-Recommendation-System/src/app/student-dashboard/student-dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-dashboard',
+  templateUrl: './student-dashboard.component.html',
+  styleUrls: ['./student-dashboard.component.css'],
+  })
+export class StudentDashboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+}
