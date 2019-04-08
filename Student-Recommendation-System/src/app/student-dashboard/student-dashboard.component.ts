477a173878944eca5e4847211314b76b0c139835
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-09 00:22:50 +0530

    Write the personal details section

--- Student-Recommendation-System/src/app/student-dashboard/student-dashboard.component.ts
+++ Student-Recommendation-System/src/app/student-dashboard/student-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Details } from '../details';
+import { DashboardService } from '../dashboard.service';
 
 @Component({
   selector: 'app-student-dashboard',
@@ -6,11 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-dashboard.component.css'],
   })
 export class StudentDashboardComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private dashboardService: DashboardService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getDetails();
+  }
 
   step = 0;
+
+  details: Details = {
+    user: '',
+    firstName: '',
+    lastName: '',
+    mobile: '',
+    email: '',
+  };
+
+  getDetails(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.dashboardService.getDashboard(id).subscribe((details: Details) => {
+      console.log(details);
+      if (details) this.details = details;
+    });
+  }
+
+  getCourses(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.dashboardService.getDashboard(id).subscribe((details: Details) => {
+      console.log(details);
+      if (details) this.details = details;
+    });
+  }
 
   setStep(index: number) {
     this.step = index;
