00949ee8d7838fea0a7d8a3a06475d9ecfef8802
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-08 23:10:21 +0530

    Add callbacks for Mid and 2nd sem

--- Student-Recommendation-System/Controllers/advisor.controller.js
+++ Student-Recommendation-System/Controllers/advisor.controller.js
@@ -1,7 +1,6 @@
 // Controller to handle all login/* routes
 /* eslint-disable no-console */
 import Student from '../Models/Student.model';
-import Grade from '../Models/Grades.model';
 
 const courses = [
   {
@@ -46,6 +45,23 @@ function compare(a, b) {
   return a.score - b.score;
 }
 
+function contains(main, sub) {
+  // if the other array is a falsy value, return false
+  if (!main) return false;
+
+  // start by assuming the array doesn't contain the thing
+  let result = false;
+  for (let i = 0, l = main.length; i < l; i += 1) {
+    // if anything in the array is the thing then change our mind from before
+
+    if (main[i] instanceof Array) {
+      if (main[i].equals(sub)) result = true;
+    } else if (main[i] === sub) result = true;
+  }
+  // return the decision we left in the variable, result
+  return result;
+}
+
 function getSuggestions(doc) {
   courses[0].score += doc.pc;
   courses[1].score += doc.dc;
@@ -69,10 +85,23 @@ exports.getAdviceForSemStart = (request, response) => {
 };
 
 exports.getAdviceForSemMid = (request, response) => {
-  if (Grade[request.params.user][request.params.id] - request.params.grade >= 2) response.send('leave');
-  else response.send('stay');
+  if (Math.floor(Math.random() * 10) + 4 - request.params.grade >= 2) return response.send('leave');
+  return response.send('stay');
 };
 
 exports.getAdviceForSem2Start = (request, response) => {
-  
+  let suggestions = [];
+  Student.findOne({ user: request.params.id }).exec((err1, doc) => {
+    if (err1) console.log(err1.message);
+    if (doc) {
+      console.log(`here1 ${doc}`);
+      courses.find({}).exec((err2, docs) => {
+        if (err1) console.log(err2.message);
+        if (docs) {
+          suggestions = docs.filter(course => contains(doc.courses, course));
+        }
+      });
+    }
+  });
+  return response.send(suggestions);
 };
