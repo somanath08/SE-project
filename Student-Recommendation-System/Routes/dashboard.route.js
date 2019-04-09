47ad4853a04e05ec11fa30e95e71bc4f0f23ff12
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-09 06:14:14 +0530

    Fix the order of routes

--- Student-Recommendation-System/Routes/dashboard.route.js
+++ Student-Recommendation-System/Routes/dashboard.route.js
@@ -3,5 +3,8 @@ import { Router } from 'express';
 const callback = require('../Controllers/dashboard.controller');
 
 const router = Router();
-router.get('/details/:user', callback.getDetails);
+router.get('/details/course/all', callback.getAllCourses);
+router.get('/details/personal/:user', callback.getPersonalDetails);
+router.get('/details/course/:user', callback.getCourseDetails);
+
 module.exports = router;
