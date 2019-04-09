38ab2f260d59a86ff13d1ec7295a4d6b427d0381
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-09 06:12:47 +0530

    Fix the query

--- Student-Recommendation-System/Controllers/dashboard.controller.js
+++ Student-Recommendation-System/Controllers/dashboard.controller.js
@@ -1,16 +1,41 @@
 // Controller to handle all login/* routes
 /* eslint-disable no-console */
 import Users from '../Models/Users.model';
+import Courses from '../Models/Course.model';
+import Student from '../Models/Student.model';
 
-exports.getDetails = (request, response) => {
+exports.getPersonalDetails = (request, response) => {
   console.log(`${JSON.stringify(request.params.user, null, 4)}`);
   Users.findOne({ user: request.params.user }).exec((err, doc) => {
     console.log(doc);
-    if (err) console.log('An error cooured while verifying');
+    if (err) console.log(err.message);
     if (doc) {
       return response.send(doc);
     }
     return response.send({});
   });
 };
-// gnbmjdmlsstwnotf;
+
+exports.getAllCourses = (request, response) => {
+  console.log('here');
+  Courses.find({}).exec((err, doc) => {
+    console.log(doc, 'yoyo');
+    if (err) console.log(err.message);
+    if (doc) {
+      return response.send(doc);
+    }
+    return response.send([]);
+  });
+};
+
+exports.getCourseDetails = (request, response) => {
+  console.log(`${JSON.stringify(request.params.user, null, 4)}`);
+  Student.find({ user: request.params.id }).exec((err, doc) => {
+    console.log(doc);
+    if (err) console.log(err.message);
+    if (doc) {
+      return response.send(doc.courses);
+    }
+    return response.send([]);
+  });
+};
