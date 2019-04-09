219861e7cb83297b21a68b719ea4e138eedeefc1
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-09 06:13:20 +0530

    Fix error

--- Student-Recommendation-System/Controllers/floatCourse.controller.js
+++ Student-Recommendation-System/Controllers/floatCourse.controller.js
@@ -3,10 +3,11 @@
 import Courses from '../Models/Course.model';
 
 exports.addCourse = (request, response) => {
+  console.log(request.body, 'float');
   const newCourse = new Courses({
     facId: request.body.facId,
     courseType: request.body.courseType,
-    courseName: request.body.coursename,
+    courseName: request.body.courseName,
     credits: request.body.credits,
     courseId: request.body.courseId,
     prerequisites: request.body.prerequisites,
