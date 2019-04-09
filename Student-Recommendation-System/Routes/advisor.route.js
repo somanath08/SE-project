dd53fada25caa570ae728bf07d85c00656838ab2
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-09 06:17:52 +0530

    Add new routes

--- Student-Recommendation-System/Routes/advisor.route.js
+++ Student-Recommendation-System/Routes/advisor.route.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 const callback = require('../Controllers/advisor.controller');
 
 const router = Router();
-router.get('/start/:id', callback.getAdiveForSemStart);
+router.get('/start/1/:id', callback.getAdiveForSemStart);
+router.get('/start/2/:id', callback.getAdviceForSem2Start);
 router.get('/mid/:user/:id/:grade', callback.getAdiveForMidSem);
 module.exports = router;
