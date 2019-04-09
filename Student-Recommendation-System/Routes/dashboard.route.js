6801e77914a9cb75182e7d8fc8ecc21573245d11
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-09 14:47:23 +0530

    Add float course api route

--- Student-Recommendation-System/Routes/dashboard.route.js
+++ Student-Recommendation-System/Routes/dashboard.route.js
@@ -7,5 +7,6 @@ router.get('/details/course/all', callback.getAllCourses);
 router.get('/details/personal/:user', callback.getPersonalDetails);
 router.get('/details/course/:user', callback.getCourseDetails);
 router.post('/details/course/add/:id', callback.addCourses);
+router.get('/details/floated/:id', callback.getFloatedCourse);
 
 module.exports = router;
