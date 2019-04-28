0ce27ddaca215487d30ba1830b87e34636c687f9
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-04-28 17:40:22 +0530

    Change route to the mid course advisor

--- Student-Recommendation-System/Routes/advisor.route.js
+++ Student-Recommendation-System/Routes/advisor.route.js
@@ -5,5 +5,5 @@ const callback = require('../Controllers/advisor.controller');
 const router = Router();
 router.get('/start/1/:id', callback.getAdviceForSemStart);
 router.get('/start/2/:id', callback.getAdviceForSem2Start);
-router.get('/mid/:user/:id/:grade', callback.getAdviceForSemMid);
+router.get('/mid/:grade', callback.getAdviceForSemMid);
 module.exports = router;
